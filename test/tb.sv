259dc45560360fe12536daca22efabb0c7d67084
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2025-04-19 01:23:34 -0300

    Update tb.sv

--- test/tb.sv
+++ test/tb.sv
@@ -1,43 +1,86 @@
 module tb;
 parameter SIZE =7, SIZEKer = 3, WIDTH_BIT = 8;
-logic clock, nreset;
+logic clock, nreset,ena;
 logic [WIDTH_BIT-1:0] inpMatrixI          [SIZE-1:0][SIZE-1:0];
 logic [WIDTH_BIT-1:0] inpMatrixIdinKer    [SIZEKer-1:0][SIZEKer-1:0];
 
 logic [WIDTH_BIT-1:0] convIxKernel ;
-logic [WIDTH_BIT-1:0] i, j ;
+logic [WIDTH_BIT-1:0] convIxKernelOut [(SIZE-SIZEKer):0][(SIZE-SIZEKer):0] ;
+
+logic [WIDTH_BIT-1:0] i, j, next,current;
+logic signed [WIDTH_BIT-1:0] k ;
 conv #(.SIZE(SIZEKer),.WIDTH_BIT(WIDTH_BIT)) CNN1 
 (
-    .clock                          ,
-    .nreset                         ,
+    .clock(clock)                          ,
+    .nreset(nreset)                         ,
     .inpMatrixI(inpMatrixIdinKer)   ,
-    .convIxKernel(convIxKernel)      
+    .convIxKernel(convIxKernel)      ,
+    .ena(ena)
 );
 
-indexMatrix #(.SIZE(SIZE-SIZEKer),.WIDTH_BIT(WIDTH_BIT)) sliced(
-    .nreset,
-    .clock,
-    .i,
-    .j
+indexMatrix #(.SIZE(SIZE-SIZEKer+1),.WIDTH_BIT(WIDTH_BIT)) sliced(
+    .nreset(nreset),
+    .clock(clock),
+    .i(i),
+    .ena(ena),
+    .j(j)
 );
 initial begin
+    // k = 0;
     $readmemb("simulation/I.txt",inpMatrixI);
     clock =0;
-    nreset = 0;
+    // ena =1;
+    #1nreset = 0;
     #1 nreset = 1;
-    repeat(40) #1 clock = ~clock;
-    $monitor("%d",convIxKernel);
+    // #1 nreset = 0;
+    // #1 nreset = 1;
+    repeat(153)begin #1 clock = ~clock;end
+    $writememh("simulation/IxKernel.txt",convIxKernelOut);
 end
-always_ff@(posedge clock)begin
-    inpMatrixIdinKer[0][0] = inpMatrixI[0+i][0+j];
-    inpMatrixIdinKer[0][1] = inpMatrixI[0+i][1+j];
-    inpMatrixIdinKer[0][2] = inpMatrixI[0+i][2+j];
-    inpMatrixIdinKer[1][0] = inpMatrixI[1+i][0+j];
-    inpMatrixIdinKer[1][1] = inpMatrixI[1+i][1+j];
-    inpMatrixIdinKer[1][2] = inpMatrixI[1+i][2+j];
-    inpMatrixIdinKer[2][0] = inpMatrixI[2+i][0+j];
-    inpMatrixIdinKer[2][1] = inpMatrixI[2+i][1+j];
-    inpMatrixIdinKer[2][2] = inpMatrixI[2+i][2+j];
-
+always_ff@(posedge clock,negedge nreset)begin
+    if(!nreset)begin
+        inpMatrixIdinKer[0][0] <= inpMatrixI[0+i][0+j];
+        inpMatrixIdinKer[0][1] <= inpMatrixI[0+i][1+j];
+        inpMatrixIdinKer[0][2] <= inpMatrixI[0+i][2+j];
+        inpMatrixIdinKer[1][0] <= inpMatrixI[1+i][0+j];
+        inpMatrixIdinKer[1][1] <= inpMatrixI[1+i][1+j];
+        inpMatrixIdinKer[1][2] <= inpMatrixI[1+i][2+j];
+        inpMatrixIdinKer[2][0] <= inpMatrixI[2+i][0+j];
+        inpMatrixIdinKer[2][1] <= inpMatrixI[2+i][1+j];
+        inpMatrixIdinKer[2][2] <= inpMatrixI[2+i][2+j];
+        ena= 0;
+        current<= 0;
+    end else begin
+        // $display("%d,%d,%d",convIxKernel,i,j);
+        case(current)
+         0:begin
+            ena <=0;
+            inpMatrixIdinKer[0][0] <= inpMatrixI[0+i][0+j];
+            inpMatrixIdinKer[0][1] <= inpMatrixI[0+i][1+j];
+            inpMatrixIdinKer[0][2] <= inpMatrixI[0+i][2+j];
+            inpMatrixIdinKer[1][0] <= inpMatrixI[1+i][0+j];
+            inpMatrixIdinKer[1][1] <= inpMatrixI[1+i][1+j];
+            inpMatrixIdinKer[1][2] <= inpMatrixI[1+i][2+j];
+            inpMatrixIdinKer[2][0] <= inpMatrixI[2+i][0+j];
+            inpMatrixIdinKer[2][1] <= inpMatrixI[2+i][1+j];
+            inpMatrixIdinKer[2][2] <= inpMatrixI[2+i][2+j];
+         end
+         1:begin 
+            ena <= 1;
+           
+         end
+         2:begin convIxKernelOut[i][j] <= convIxKernel;
+            $display("%d,%d,%d,%t",convIxKernel,i,j,$time);
+            ena <= 0;
+            
+         end
+        endcase
+        current <= next;
+    end
 end
+    always_comb case(current)
+        0:next = 1;
+        1:next = 2;
+        2:next = 0;
+    endcase
 endmodule
