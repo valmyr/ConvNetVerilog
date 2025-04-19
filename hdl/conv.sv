caf24fd50fbd612b3a000e1eef0e6e9356b4f0de
valmyr <valmir.silva@ee.ufcg.edu.br>
2025-04-19 04:28:56 -0300

    novo nível de hierarquia

--- hdl/conv.sv
+++ hdl/conv.sv
@@ -35,49 +35,18 @@ module conv#(parameter SIZE = 3,WIDTH_BIT = 8)(
         end
         for(i =0; i< SIZE; i=i+1)begin:SUM_HOR
             for(j = 0; j < SIZE; j++)begin:SUM_HOR
-                if(j == 0)
+                if(!j)
                     assign SumHoriz[i][0] = ProducMx[i][0];
                 else
                     sum s(SumHoriz[i][j-1],ProducMx[i][j],SumHoriz[i][j]);
             end
         end
-
         for(i = 0; i < SIZE; i++)begin:SUM_VERT
-            if(i == 0)assign SumVertic[0] = SumHoriz[0][2];
+            if(!i)assign SumVertic[0] = SumHoriz[0][2];
             else sum si(SumVertic[i-1],SumHoriz[i][2],SumVertic[i]);
         end
     endgenerate
-    // assign convIxKernel = O[0][2]+O[1][2]+O[2][2];
     assign convIxKernel = SumVertic[2];
 
 
 endmodule
-
-/*
-    sum s(O[0][1-1],W[0][1],O[0][1]);
-    sum s(O[0][2-1],W[0][2],O[0][2]);
-
-
-    O[0],W[1],O[1]
-    O[1],W[2],O[2]
-    O[2],W[3],O[3]
-    O[3],W[4],O[4]
-    O[4],W[5],O[5]
-    O[5],W[6],O[6]
-    O[6],W[7],O[7]
-    O[7],W[8],O[8]
-
-
-
-    00 01 02 
-    10 11 12
-    20 21 22
-    
-    11 12 13
-    21 22 23
-    31 32 33
-
-    2 3 4
-    3 4 5 
-    4 5 6
-*/
