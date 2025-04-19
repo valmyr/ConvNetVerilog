d6563b7540bba322d82cdb388a8e68d83ddf2397
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2025-04-18 22:03:36 -0300

    Delete xcelium.d directory

--- xcelium.d/run.lnx8664.23.09.d/files.ts
+++
@@ -1,3 +0,0 @@
-1745024318 /home/valmir.silva/ConvolutionMatrixVerilog/test/tb.sv
-1745024197 /home/valmir.silva/ConvolutionMatrixVerilog/hdl/cnn.sv
-1745021869 /home/valmir.silva/ConvolutionMatrixVerilog/hdl/indexMatrix.sv
